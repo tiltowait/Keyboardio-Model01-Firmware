52cbd8ed71ed2e001b172c644811b55f3d860c25
tiltowait <tiltowaitt+github@icloud.com>
2019-01-08 17:21:39 -0800

    Pretty up keymap entries and use KEYMAPS macro

--- src/keymaps.h
+++ src/keymaps.h
@@ -77,29 +77,28 @@ enum { QWERTY, FUNCTION, NUMPAD }; // layers
  */
 // *INDENT-OFF*
 
-const Key keymaps[][ROWS][COLS] PROGMEM = {
-
+KEYMAPS(
   [QWERTY] = KEYMAP_STACKED
-  (___,          Key_1, Key_2, Key_3, Key_4, Key_5, KEY_DEL_LINE,
-   Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
-   Key_PageUp,   Key_A, Key_S, Key_D, Key_F, Key_G,
-   Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
+  (___,             Key_1,         Key_2,       Key_3,         Key_4, Key_5, KEY_DEL_LINE,
+   Key_Backtick,    Key_Q,         Key_W,       Key_E,         Key_R, Key_T, Key_Tab,
+   Key_PageUp,      Key_A,         Key_S,       Key_D,         Key_F, Key_G,
+   Key_PageDown,    Key_Z,         Key_X,       Key_C,         Key_V, Key_B, Key_Escape,
    Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
    ShiftToLayer(FUNCTION),
 
-   KEY_LAUNCHBAR,  Key_6, Key_7, Key_8,     Key_9,         Key_0,         LockLayer(NUMPAD),
-   Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
-                  Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
-   KEY_EMDASH,  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
+   KEY_LAUNCHBAR,  Key_6,     Key_7,        Key_8,            Key_9,      Key_0,         LockLayer(NUMPAD),
+   Key_Enter,      Key_Y,     Key_U,        Key_I,            Key_O,      Key_P,         Key_Equals,
+                   Key_H,     Key_J,        Key_K,            Key_L,      Key_Semicolon, Key_Quote,
+   KEY_EMDASH,     Key_N,     Key_M,        Key_Comma,        Key_Period, Key_Slash,     Key_Minus,
    Key_RightShift, Key_Enter, Key_Spacebar, Key_RightControl,
    ShiftToLayer(FUNCTION)),
 
   [FUNCTION] =  KEYMAP_STACKED
-  (KEY_1PASSWORD,      Key_F1,           Key_F2,      Key_F3,     Key_F4,        Key_F5,           Key_LEDEffectNext,
-   Key_Tab,  ___,              Key_mouseUp, ___,        Key_mouseBtnR, Key_mouseWarpEnd, Key_mouseWarpNE,
-   Key_Home, Key_mouseL,       Key_mouseDn, Key_mouseR, Key_mouseBtnL, Key_mouseWarpNW,
-   Key_End,  Key_PrintScreen,  Key_Insert,  ___,        Key_mouseBtnM, Key_mouseWarpSW,  Key_mouseWarpSE,
-   ___, Key_Delete, ___, ___,
+  (KEY_1PASSWORD, Key_F1,          Key_F2,      Key_F3,     Key_F4,        Key_F5,           Key_LEDEffectNext,
+   Key_Tab,       ___,             Key_mouseUp, ___,        Key_mouseBtnR, Key_mouseWarpEnd, Key_mouseWarpNE,
+   Key_Home,      Key_mouseL,      Key_mouseDn, Key_mouseR, Key_mouseBtnL, Key_mouseWarpNW,
+   Key_End,       Key_PrintScreen, Key_Insert,  ___,        Key_mouseBtnM, Key_mouseWarpSW,  Key_mouseWarpSE,
+   ___,           Key_Delete,      ___,         ___,
    ___,
 
    Consumer_ScanPreviousTrack, Key_F6,                 Key_F7,                   Key_F8,                   Key_F9,          Key_F10,          Key_F11,
@@ -108,7 +107,6 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
    Key_PcApplication,          Key_Mute,               Consumer_VolumeDecrement, Consumer_VolumeIncrement, ___,             Key_Backslash,    Key_Pipe,
    ___, ___, Key_Enter, ___,
    KEY_THINGS),
-
 
   [NUMPAD] =  KEYMAP_STACKED
   (___, ___, ___, ___, ___, ___, ___,
@@ -124,7 +122,7 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
    ___,                    ___, Key_Keypad0, Key_KeypadDot, Key_KeypadMultiply, Key_KeypadDivide,   Key_Enter,
    ___, ___, ___, ___,
    ___)
-};
+);
 
 /* Re-enable astyle's indent enforcement */
 // *INDENT-ON*
